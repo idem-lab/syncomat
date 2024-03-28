f980f744ed190d9243415a4e76005a8f45cfd0ac
njtierney <nicholas.tierney@gmail.com>
2024-03-28 16:06:17 +1100

    more misc notes from todays meeting

--- R/functions.R
+++ R/functions.R
@@ -1,68 +1,79 @@
-create_country_list_from_wpp <- function(wpp_data){
-  
+create_country_list_from_wpp <- function(wpp_data,
+                                         countries = all_countries){
+
   # Returns all countries in the dataset in list form.
-  
-  all_countries <- read_csv("./raw/all-countries.csv") %>% 
-    select(country = name)
-  
-  wpp_countries <- wpp_data %>% 
+  wpp_countries <- wpp_data %>%
     distinct(country)
-  
-  list_of_countries <- inner_join(wpp_countries, all_countries, by = "country")
-  list_of_countries <- list_of_countries %>% 
-    pull(country) %>% 
-    as.character
-  
+
+  list_of_countries <- inner_join(wpp_countries, countries, by = "country")
+  list_of_countries <- list_of_countries %>%
+    pull(country) %>%
+    as.character()
+
   list_of_countries
 }
 
 create_pop_data <- function(country_list){
-  
+
   # This function takes the list of countries,
-  # plugs it into wpp_age() 
+  # plugs it into wpp_age()
   # and subsequently as_conmat_population()
   # to derive the population data.
-  
-  data <- map(country_list, 
-              \(x) wpp_age(x, "2015")) %>% 
+
+  data <- map(country_list,
+              \(x) wpp_age(x, "2015")) %>%
     set_names(country_list)
-  
-  data_pop <- map(data, 
-                  \(x) as_conmat_population(x, 
-                                            age = lower.age.limit, 
-                                            population = population)) %>% 
+
+  data_pop <- map(data,
+                  \(x) as_conmat_population(x,
+                                            age = lower.age.limit,
+                                            population = population)) %>%
     set_names(country_list)
-  
+
   data_pop
 }
 
-create_contact_matrices <- function(data_pop, country_list, start_age = 0, end_age = 80){
-  
+create_contact_matrices <- function(data_pop,
+                                    country_list,
+                                    start_age = 0,
+                                    end_age = 80){
+
   # This function takes the population data and the list of countries
   # derived from as_conmat_population()
   # and subsequently uses extrapolate_polymod()
   # to derive the social contact matrices
-  
+
   age_breaks_user_defined <- c(seq(start_age, end_age, by = 5), Inf)
-  
-  data_contact <- map(data_pop,
-                      \(x) extrapolate_polymod(x, 
-                                               age_breaks = age_breaks_user_defined)) %>% 
+
+  data_contact <- map(
+    .x = data_pop,
+    .f = \(x) extrapolate_polymod(
+      population = x,
+      age_breaks = age_breaks_user_defined
+      )
+    ) %>%
     set_names(country_list)
-  
+
   data_contact
 }
 
 save_conmat_as_csv <- function(matrix_list, path = "./", subfolder = FALSE) {
-  
+
   # This function saves the contact matrices derived from
   # extrapolate_polymod() for a list of countries and
   # saves them to individual csv files.
   # Option to save to subfolders.
-  
+  n_countries <- length(names(matrix_list))
+  n_matrices <- sum(lengths(matrix_list))
+  total_file_paths <- n_countries * n_matrices
+  new_file_path <- vector("character", length = total_file_paths)
+
+  ## TODO
+  ## Think about moving some of these sections of the for loop
+  ## outside of the for loop - there might be vectorised
+  ## ways to do some of these steps
   for (country_name in names(matrix_list)) {
     country_matrices <- matrix_list[[country_name]]
-    
     # Create subfolder for each country if subfolder = TRUE, else remain same
     if (subfolder) {
       folder_location <- file.path(path, country_name)
@@ -70,12 +81,14 @@ save_conmat_as_csv <- function(matrix_list, path = "./", subfolder = FALSE) {
     } else {
       folder_location <- path
     }
-    
+
     for (matrix_name in names(country_matrices)) {
       matrix_data <- country_matrices[[matrix_name]]
       file_name <- glue("{country_name}_{matrix_name}_2015.csv")
       file_path <- file.path(folder_location, file_name)
+      # new_file_path[matrix_name] <- file_path
       write.csv(matrix_data, file_path, row.names = TRUE)
     }
   }
+  # new_file_path
 }
--- _targets.R
+++ _targets.R
@@ -4,11 +4,21 @@ library(targets)
 library(tarchetypes)
 
 tar_option_set(
-  packages = c("conmat", "socialmixr", "mgcv",
-               "tibble", "readr", "dplyr", "ggplot2", 
-               "patchwork", "glue", "fs",
+  packages = c("conmat",
+               "socialmixr",
+               "mgcv",
+               "tibble",
+               "readr",
+               "dplyr",
+               "ggplot2",
+               "patchwork",
+               "glue",
+               "fs",
                "purrr")
 )
+
+# this will setup a workspace when our code errors
+tar_option_set(workspace_on_error = TRUE)
 
 source("R/functions.R")
 
@@ -17,26 +27,54 @@ source("R/functions.R")
 # To make dataset from wpp_age() only
 
 tar_plan(
-  
+
   # Extract all data from wpp_age()
   tar_target(wpp_data, wpp_age()),
-  
+
+  tar_file(
+    all_countries_path,
+    "raw/all-countries.csv"
+  ),
+
+  tar_target(
+    all_countries,
+    read_csv(all_countries_path) %>% select(country = name)
+  ),
+
   # Create country_list
-  tar_target(country_list, create_country_list_from_wpp(wpp_data)),
-  
+  tar_target(country_list,
+             create_country_list_from_wpp(
+               wpp_data = wpp_data,
+    countries = all_countries)),
+
+  # subset the country list
+  tar_target(
+    proposed_countries,
+    country_list[131:134]
+  ),
+
   # In the following two targets,
   # choose which countries you'd like to extract data for using the index
-  
+
   # Create population data from list of countries
-  tar_target(data_pop, create_pop_data(country_list[131:183])),
-  
+  tar_target(data_pop, create_pop_data(proposed_countries)),
+
   # Create contact matrices
-  tar_target(data_contact, 
-             create_contact_matrices(data_pop, country_list[131:183], 0, 80)),
-  
+  tar_target(data_contact,
+             create_contact_matrices(
+               data_pop = data_pop,
+               country_list = proposed_countries,
+               start_age = 0,
+               end_age = 80
+               )),
+
   # Save the csv files
   tar_target(csv_output,
-             save_conmat_as_csv(data_contact, path = "./output/240326 all", subfolder = TRUE), 
+             save_conmat_as_csv(
+               matrix_list = data_contact,
+               path = "./output/240326 all",
+               subfolder = TRUE
+               ),
              format = "file")
-  
+
 )
