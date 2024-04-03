e1cdf55bc5505e561f9144b39fa45ce4870f3e89
chitra.m.saraswati@gmail.com <chitra.m.saraswati@gmail.com>
2024-04-03 09:07:48 +0800

    How to do pull requests in Git (happygitwithR)

---
+++ scripts/exp-git_pull_requests.R
@@ -0,0 +1,4 @@
+library(usethis)
+pr_fetch(9) # this will get my PR and load it into your repo
+tar_make() # check that the outputs and stuff work locally
+# try other random tests locally to see if it is all working
